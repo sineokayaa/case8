0f7906a8fa8d4425f4b46d8279e1cf0d8a2b6b07
sineokayaa <a.sineokaya@g.nsu.ru>
2024-04-14 16:59:08 +0700

    3

--- 1.py
+++ 1.py
@@ -1,22 +1,26 @@
 class Hotel:
+    price_of_room = {}
     nums = []
     types = []
     max_ppls = []
     comforts = []
+    booked_dates = []
     price_type = {'одноместный': 2900, 'двухместный': 2300, 'полулюкс': 3200, 'люкс': 4100}
     comfort_price = {'стандарт': 1, 'стандарт_улучшенный': 1.2, 'апартамент': 1.5}
     food_price = {'без питания': 0, 'завтрак': 280, 'полумансион': 1000}
 
     def __init__(self, ptr):
         ptr = ptr.split()
-        self.num = ptr[0]
+        self.num = int(ptr[0])
         self.type = ptr[1]
-        self.max_ppl = ptr[2]
+        self.max_ppl = int(ptr[2])
         self.comfort = ptr[3]
         Hotel.nums.append(self.num)
         Hotel.types.append(self.type)
         Hotel.max_ppls.append(self.max_ppl)
         Hotel.comforts.append(self.comfort)
+        Hotel.booked_dates.append([])
+        Hotel.price_of_room[self.num] = Hotel.price_type[self.type] * self.max_ppl * Hotel.comfort_price[self.comfort]
 
 
 with open('fund.txt', encoding='utf-8') as f:
@@ -47,8 +51,13 @@ class Clients:
         Clients.sums.append(self.sum)
 
 
+
+
+
+
+
 with open('booking.txt', encoding='utf-8') as f:
     for n in f:
         Clients(n)
 
-
+print(Hotel.price_of_room)
